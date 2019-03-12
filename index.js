8dad3ffa0712ed0c06a7be6fde9ca8d90cf92ece
Tami Churns <tchurns@paperlesspost.com>
2019-03-12 00:42:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+const headquartersBlock = 42
+const feetPerBlock = 264
+
+function distanceFromHqInBlocks(block) {
+  return Math.abs(block - headquartersBlock)
+}
+
+function distanceFromHqInFeet(block) {
+  return distanceFromHqInBlocks(block) * feetPerBlock
+}
+
+function distanceTravelledInFeet(block1, block2) {
+  return Math.abs((block1 * feetPerBlock) - (block2 * feetPerBlock))
+}
+
+function calculatesFarePrice(start, destination) {
+  if (distanceTravelledInFeet(start, destination) < 400) { return 0 
+  } else if (sistanceTravelledInFeet(start, destination) >= 400 && distanceTravelledInFeet(start,destination) < 20000) }
+}
